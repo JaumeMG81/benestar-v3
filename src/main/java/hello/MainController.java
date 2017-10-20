cf4a8941efe8faf816a0148695ae1b17d576f103
ITE-JMALAGARRIG\jmalagarriga <r11j14@gmail.com>
2017-10-20 12:50:01 +0200

    no message

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -35,4 +35,10 @@ public class MainController {
 		// This returns a JSON or XML with the users
 		return userRepository.findAll();
 	}
+	
+	@GetMapping(path="/hola")
+	public @ResponseBody String getHola() {
+		// This returns a JSON or XML with the users
+		return "hola";
+	}
 }
