32b62b0e0bc4b6f409e3cd4541ef74ebfc808309
ITE-JMALAGARRIG\jmalagarriga <r11j14@gmail.com>
2017-10-20 10:56:25 +0200

    no message

---
+++ src/main/java/hello/User.java
@@ -0,0 +1,50 @@
+package hello;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "usuari")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "nom", length = 30)
+    @NotNull
+    private String name;
+
+    @Column(name = "correu", length = 30)
+    @NotNull
+    private String email;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
+
